707785687801fb50a8494630eb7a0abb7cf9f811
Amitabh <amitabh@codelogicx.com>
2018-01-12 19:55:07 +0530

    made links dynamic

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config['development'])
+    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
